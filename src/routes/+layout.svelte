94f0e59bcea88f6c50155eae41f8377dbe10b8a1
Wayne Allan <wayne@jellybeanjar.io>
2023-12-30 08:38:22 +1100

    feat: add docs and navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,33 +14,11 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Swimming Stats</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/"> API </a>
+				<a class="btn btn-sm variant-ghost-surface" href="/data/competitors"> Data </a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
